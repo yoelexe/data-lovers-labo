482dd646e59be5feaaa1edb6a0e2e32bb67b0f8d
Karen Huamán <karmahucents@gmail.com>
2023-03-07 13:14:20 -0500

    Squashed commit of the following:
    commit a66f236eac45f4a41f870044e17da1efcdb70d56
    Author: yoelexe <karmahucents@gmail.com>
    Date:   Tue Mar 7 12:53:51 2023 -0500
    
        probando juntar código
    
    commit 590c747585ea58b22a5e07fe4b90f3274cd1df86
    Author: yoelexe <karmahucents@gmail.com>
    Date:   Fri Mar 3 12:36:53 2023 -0500
    
        probando github colaborativo
    
    commit 283b3142536e9d89ace9f8063e15fd63c94f4085
    Author: yoelexe <karmahucents@gmail.com>
    Date:   Fri Mar 3 12:28:31 2023 -0500
    
        probando cambios en github

--- src/main.js
+++ src/main.js
@@ -1,6 +1,50 @@
-import { example } from './data.js';
+import { filterData, example } from './data.js';
+// import data from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/harrypotter/harry.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+const search_wand = document.querySelector('#search-wand');
+const box_grid =document.querySelector('#box-grid');
+
+const dataHarry = data.spells;
+
+search_wand.addEventListener('input', () => {
+  //const name = document.getElementById('search-wand').value;
+  const filtrarDataByName = window.filterData(search_wand);
+  printData(filtrarDataByName)
+  console.log(filterData)
+  
+
+})
+
+const printData = (data) => {
+  let str = '';
+  data.array.forEach(e => {
+    str +=  `<div class="box">
+    <h3>Nombre: ${e.name}</h3>
+    <p>Descripción: ${e.description}</p>
+    </div>`;
+    return data;
+  });
+  box_grid.innerHTML = str;
+}
+
+
+// Mostrar el template del html pero js
+const template = (list) => {
+  let templateList = '';
+  list.forEach((dataHarry) => {
+    const card = `<div class="box">
+    <h3>Nombre: ${dataHarry.name}</h3>
+    <p>Descripción: ${dataHarry.description}</p>
+    </div>`;
+    templateList += card;
+  })
+  document.getElementById('box-grid').innerHTML = templateList;
+}
+template(dataHarry);
+
+data.spells.map(wind => console.log(wind.name + ' : ' + wind.spell_type))
+console.log(example);
+console.log(data.spells.filter(spells => spells.name === 'Accio').map(wind => wind.name + ' : ' + wind.spell_type))
