4c4bbaa4de40a34e4dd1faa15ee2b39c6d2755a8
adrieli2307 <aearmijosq1104@gmail.com>
2023-03-17 09:53:53 -0500

    Eliminacion span en botomes dependiendo la seccion

--- src/main.js
+++ src/main.js
@@ -10,6 +10,18 @@ botones.forEach(function (elemento) {
   elemento.addEventListener('click', (event) => {
     let seccion = event.currentTarget.dataset.section;
     let secciones = document.getElementsByTagName("section");
+
+    if (seccion != 'section1') {
+      // Ocultar texto de los botones
+      document.querySelectorAll("button span").forEach((span) => {
+        span.style.display = 'none'
+      })
+    } else {
+      document.querySelectorAll("button span").forEach((span) => {
+        span.style.display = 'inline'
+      })
+    }
+
     for (let i = 0; i < secciones.length; i++) {
       secciones[i].style.display = "none";
     }
@@ -23,3 +35,4 @@ botones.forEach(function (elemento) {
 
 /* Llamor la funcion desde data js*/
 baseDatos()
+
