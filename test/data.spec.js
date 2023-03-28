f4edaaeaf066d6b4c1f476585ab76a40235abc66
adrieli2307 <aearmijosq1104@gmail.com>
2023-03-28 12:00:13 -0500

    Actualizacion tests

--- test/data.spec.js
+++ test/data.spec.js
@@ -26,7 +26,7 @@ const dataspell = [
 
 //TODO: Primer testeo
 describe("Buscador de pociones", () => {
-  it("Deberia retornar que es una función -> filtroHechizo", () => {
+  it("Deberia retornar que es una función -> filterByName", () => {
     expect(typeof filterByName).toBe("function");
   });
 
@@ -69,10 +69,48 @@ describe("Filtrar por tipo de hechizo", () => {
   });
 });
 
+const dataCharacter = [
+  {
+    id: 50,
+    name: "Sirius Black",
+    house: "Gryffindor",
+  },
+  {
+    id: 117,
+    name: "Cedric Diggory",
+    house: "Hufflepuff",
+  },
+  {
+    id: 242,
+    name: "Lestrange",
+    house: "Slytherin",
+  },
+  {
+    id: 367,
+    name: "Newton Scamander",
+    house: "Hufflepuff",
+  },
+  {
+    id: 625,
+    name: "Luna Lovegood",
+    house: "Ravenclaw",
+  },
+];
+
 //TODO: Tercer testeo
 describe("Filtrar los personajes por casas", () => {
   it("Deberia retornar que es una función -> changeHouse", () => {
     expect(typeof changeHouse).toBe("function");
+  });
+
+  it("Deberia retornar el arreglo por cada tipo de casa", () => {
+    expect(changeHouse(dataCharacter, "Slytherin")).toEqual([
+      {
+        id: 242,
+        name: "Lestrange",
+        house: "Slytherin",
+      },
+    ]);
   });
 });
 
@@ -81,11 +119,83 @@ describe("Buscador de personajes", () => {
   it("Deberia retornar que es una función -> getHouse", () => {
     expect(typeof getHouse).toBe("function");
   });
+
+  it("Deberia retornar el arreglo por cada tipo de casa", () => {
+    expect(getHouse(dataCharacter, "Luna Lovegood")).toEqual([
+      {
+        id: 625,
+        name: "Luna Lovegood",
+        house: "Ravenclaw",
+      },
+    ]);
+  });
 });
+
+const dataPotions = [
+  {
+    id: 3,
+    name: "Amortentia",
+  },
+  {
+    id: 31,
+    name: "Cough Potion",
+  },
+  {
+    id: 35,
+    name: "Death potion",
+  },
+];
 
 //TODO: Quinto testeo
 describe("Ordenar pociones ascendente y descendente", () => {
   it("Deberia retornar que es una función -> orderByValue", () => {
     expect(typeof orderByValue).toBe("function");
   });
+
+  it("Deberia retornar un arreglo de forma descendente -> orderByValue", () => {
+    expect(orderByValue(dataPotions, "Descendente")).toEqual([
+      {
+        id: 35,
+        name: "Death potion",
+      },
+      {
+        id: 31,
+        name: "Cough Potion",
+      },
+      {
+        id: 3,
+        name: "Amortentia",
+      },
+    ]);
+  });
+
+  it("Deberia retornar un arreglo de forma ascendente -> orderByValue", () => {
+    expect(orderByValue(dataPotions, "Ascendente")).toEqual(dataPotions);
+  });
+});
+
+//TODO: Sexto testeo
+describe('changeHouse', () => {
+  const characters = [
+    { name: 'Harry Potter', house: 'Gryffindor' },
+    { name: 'Draco Malfoy', house: 'Slytherin' },
+    { name: 'Luna Lovegood', house: 'Ravenclaw' },
+    { name: 'Cedric Diggory', house: 'Hufflepuff' },
+  ];
+
+  test('devuelve todos los personajes si no se especifica una casa', () => {
+    const valorcasas = '';
+    const resultados = changeHouse(characters, valorcasas);
+    expect(resultados).toEqual(characters);
+  });
+
+  test('devuelve solo los personajes de la casa especificada', () => {
+    const valorcasas = 'Gryffindor';
+    const resultados = changeHouse(characters, valorcasas);
+    const personajesEsperados = [
+      { name: 'Harry Potter', house: 'Gryffindor' },
+    ];
+    expect(resultados).toEqual(personajesEsperados);
+  });
+
 });
